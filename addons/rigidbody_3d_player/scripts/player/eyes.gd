6d81248bfc362ee901176a73877e3245053ac6de
Ivan <ivanbabaev217@gmail.com>
2026-06-13 20:27:45 +0800

    Minor refactor

--- addons/rigidbody_3d_player/scripts/player/eyes.gd
+++ addons/rigidbody_3d_player/scripts/player/eyes.gd
@@ -2,9 +2,9 @@ class_name Eyes
 extends Camera3D
 
 @export_group("Nodes")
-@export var player: Player
+@export var player_head: Node3D
 
 
 func _process(delta: float) -> void:
-	global_position = player.head.get_global_transform_interpolated().origin
-	global_rotation = player.head.global_rotation
+	global_position = player_head.get_global_transform_interpolated().origin
+	global_rotation = player_head.global_rotation
